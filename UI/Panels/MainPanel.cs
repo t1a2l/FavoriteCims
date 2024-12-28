36de7891dc58700e7cfac38f1d39549a2095d1a2
Will <125041335+will258012@users.noreply.github.com>
2024-12-28 16:07:56 +0800

    Fix filename

--- UI/Panels/MainPanel.cs
+++ UI/Panels/MainPanel.cs
@@ -127,7 +127,7 @@ namespace FavoriteCims.UI.Panels
                 }
                 opacity = 1f;
             };
-            Texture texture2 = ResourceLoader.LoadTexture((int)width, 58, "UIMainPanel.title.png");
+            Texture texture2 = ResourceLoader.LoadTexture((int)width, 58, "UIMainPanel.favcimstitle.png");
             texture2.wrapMode = TextureWrapMode.Clamp;
             texture2.filterMode = FilterMode.Bilinear;
             texture2.mipMapBias = -0.5f;
