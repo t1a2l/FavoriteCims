a267076e5b97ee54b2a7e8a1cd210056968ec1b2
Tal <talrofeh@gmail.com>
2025-01-18 07:18:01 +0200

    change version

--- FavoriteCimsModMain.cs
+++ FavoriteCimsModMain.cs
@@ -8,8 +8,11 @@ namespace FavoriteCims
     public sealed class FavoriteCimsModMain : OptionsMod<OptionsPanel>, IUserMod
     {
         public override string BaseName => "Favorite Cims";
+
         public override string LogName => "FavoriteCimsMod";
+
         public string Description => Translations.Translate("MOD_DESCRIPTION");
+
         public override void LoadSettings() => FavCimsSettings.Load();
 
         public override void SaveSettings() => FavCimsSettings.Save();
